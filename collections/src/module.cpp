4b77fc0131fe7b09c7e1c7158a8eefdeaa09c86f
Daniel Grose <31761607+grosed@users.noreply.github.com>
2024-03-22 14:35:03 +0000

    Add files via upload

--- collections/src/module.cpp
+++ collections/src/module.cpp
@@ -2,72 +2,8 @@
 #include "Rcpp.h"
 using namespace Rcpp;
 
-// [[Rcpp::plugins("cpp17")]]
-
-#include <string>
-#include <iostream>
-#include <complex>
-#include <list>
-#include <map>
-#include <set>
-#include <any>
 
 #include <deque>
-
-
-
-
-
-
-std::string marshall_string(const std::string& X)
-{
-std::any a(10);
-    Rcout << X << std::endl;
-    std::string Y {" blady blah ..."};
-    return X + Y;
-}
-
-template <typename T>
-T marshall_T_T(const T& X)
-{
-    return X;
-}
-
-template <typename T>
-void marshall_T_void(const T& X)
-{
-}
-
-
-template <typename T>
-T marshall_double_T(const double& X)
-{
-  T Y;
-  return Y;
-}
-
-
-RCPP_MODULE(marshalling) 
-{
-// function("rcpp_marshall_string", &marshall_string);
-
-
-function("rcpp_marshall_list_list_double", &marshall_T_T<std::list<std::list<double> > >);
-function("rcpp_marshall_list_vector_double", &marshall_T_T<std::list<std::vector<double> > >);
-function("rcpp_marshall_vector_list_double", &marshall_T_T<std::vector<std::list<double> > >);
-function("rcpp_marshall_vector_vector_double", &marshall_T_T<std::vector<std::vector<double> > >);
-
-function("rcpp_marshall_vector_double", &marshall_T_T<std::vector<double> > );
-function("rcpp_marshall_list_double", &marshall_T_T<std::list<double> > );
-
-
-function("rcpp_marshall_vector_double_void", &marshall_T_void<std::vector<double> > );
-
-function("rcpp_marshall_double_map_string_double", &marshall_double_T<std::map<std::string,double> > );
-function("rcpp_marshall_double_set_double", &marshall_double_T<std::set<double> > );
-function("rcpp_marshall_double_unordered_set_double", &marshall_double_T<std::unordered_set<double> > );
-
-}
 
 
 void deque_push_back(std::deque<SEXP>* obj, SEXP x )
@@ -107,12 +43,14 @@ SEXP deque_at(std::deque<SEXP>* obj, const int& i)
   return obj->at(i-1);
 }
 
+void deque_dispatch(std::deque<SEXP>* obj)
+{
+  
+}
+
 
 RCPP_MODULE(collections) 
 {
-function("rcpp_marshall_string", &marshall_string);
-
-
 class_<std::deque<SEXP> >("deque")
 .constructor()
 .method("push_back", &deque_push_back)
@@ -122,6 +60,7 @@ class_<std::deque<SEXP> >("deque")
 .method("front", &deque_front)
 .method("back", &deque_back)
 .method("at", &deque_at)
+.method("dispatch", &deque_dispatch)
 ;
 }
 
